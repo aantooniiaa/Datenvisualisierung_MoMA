926cdbbbd0ed7eaeb8e0ab9c2c4ef796db4f3a43
aantooniiaa <antonia.gruber@hfg.design>
2021-06-17 13:12:05 +0200

    neues main

--- Datenvisualisierung MomA/main.js
+++ Datenvisualisierung MomA/main.js
@@ -1,245 +1,225 @@
-// let stageWidth, stageHeight; 
-// let data;
-// let womenCountries;
-// let menCountries;
+//zT fehlen Daten in allCountries? zB Länder die nur weibliche Künstler haben fehlen
+//zT gibt es gender die groß und kleingeschrieben werden und es gibt non binory gender und welche ohne gender --> totalCount ist größer als female und male count zsm
 
 
-// $ (function() {
-//     stageWidth = $('#stage').innerWidth();
-//     stageHeight = $('#stage').innerHeight();
-//     prepareData(); 
-//     //createDots();
-//     drawAll();
-//     drawWomen();
-//     drawMen();
-// });
-
-// function prepareData() {
-
-//  data = gmynd.mergeData(artistData, countryNationalityData, "nationality"); // in Künstler Datensatz werden Herkunftsländer hinzugefügt
- 
-//  allCountries = gmynd.mergeData(positionData, countryNationalityData, "country", "countryName"); //in Länderdatensatz wird Nationalität eingebunden
-
-//  let cumulatedArtists = gmynd.cumulateData(data, ["nationality"]); //Künstler werden nach Nationalität gefiltert und gezählt
-//  allCountries = gmynd.mergeData(allCountries, cumulatedArtists, "nationality"); //"count" (Anzahl Künstler pro Land) wird zu restlichen Landinfos hinzugefügt
-
-//  let cumulatedGender = gmynd.cumulateData(data, ["nationality", "gender"]); //Künstlerinnen werden nach Nationalität und Gender gefiltert und gezählt
-//  cumulatedGender = gmynd.groupData(cumulatedGender, "gender"); //KünstlerInnen werden nach Gender sortiert
-//  womenCountries = cumulatedGender.Female; //Datensatz mit Künstlerinnen sortiert nach Nationalität
-//  womenCountries = gmynd.mergeData(womenCountries, allCountries, "nationality"); //Koordinaten werden zu sortierten weiblichen Ländern hinzugefügt
-//  menCountries = cumulatedGender.Male; //Datensatz mit Künstler sortiert nach Nationalität
-//  menCountries = gmynd.mergeData(menCountries, allCountries, "nationality"); //Koordinaten werden zu sortierten männlichen Ländern hinzugefügt
+let stageWidth, stageHeight;
+let data;
+let allCountries;
+let womenCountries;
+let menCountries;
+let totalMapShow;
+let femaleMapShow;
+let maleMapShow;
 
 
+$(function () {
+    stageWidth = $('#stage').innerWidth();
+    stageHeight = $('#stage').innerHeight();
+    prepareData();
+    createDots();
+    drawTotalMap();
+});
 
-//  console.log(womenCountries);
-// };
+function prepareData() {
 
+    data = gmynd.mergeData(artistData, countryNationalityData, "nationality"); // in Künstler Datensatz werden Herkunftsländer hinzugefügt
 
-// // function drawMap() { 
-
-// //   const artistsMax = gmynd.dataMax(allCountries, "count");
-// //   //console.log(artistsMax);
-
-// //   allCountries.forEach(country => {
-// //      const area = gmynd.map(country.count, 0, artistsMax, 25, 1000);
-// //      const r =  gmynd.circleRadius(area);
-// //       //const area = 20;
-// //       //const r =  gmynd.circleRadius(area);
-// //       const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) -r;  //Längengrade
-// //       const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
-      
-// //       let dot = $ ('<div></div>');
-// //       dot.addClass("country");
-// //         dot.css({
-// //             'height': r * 2,
-// //             'width': r * 2,
-// //             'left': x,
-// //             'top': y,
-// //         });
-
-// //         dot.data(country);  //Daten in HTML Objekten speichern
-
-       
-
-// //         dot.mouseover(() => {
-// //             dot.addClass("hover");
-// //             $('#hoverLabel').text(country.countryName);
-// //         });
-
-// //         dot.mouseout(() => {
-// //             dot.removeClass("hover");
-// //             $('#hoverLabel').text("");
-// //         });
-
-// //         $('#stage').append(dot);
+    let countryData = gmynd.mergeData(positionData, countryNationalityData, "country", "countryName"); //in Länderdatensatz wird Nationalität eingebunden
+    countryData = gmynd.deleteProps(countryData, ["alpha2Code", "numericCode"]); //löscht die angegebenen Parameter raus
 
 
-// //     })
-// // };
+    let cumulatedArtists = gmynd.cumulateData(data, ["nationality"]); //Künstler werden nach Nationalität gefiltert und gezählt
+    allCountries = gmynd.mergeData(countryData, cumulatedArtists, "nationality"); //"count" (Anzahl Künstler pro Land) wird zu restlichen Landinfos hinzugefügt
 
-// // function createDots() { 
+    let cumulatedGender = gmynd.cumulateData(data, ["nationality", "gender"]); //Künstlerinnen werden nach Nationalität und Gender gefiltert und gezählt
+    cumulatedGender = gmynd.groupData(cumulatedGender, "gender"); //KünstlerInnen werden nach Gender sortiert
 
-// //     let dot = $ ('<div></div>');
-// //         dot.addClass("country");
-// //           dot.css({
-// //               'height': r * 2,
-// //               'width': r * 2,
-// //               'left': x,
-// //               'top': y,
-// //           });
-  
-// //           dot.data(country);  //Daten in HTML Objekten speichern
-  
-// //           dot.mouseover(() => {
-// //               dot.addClass("hover");
-// //               $('#hoverLabel').text(country.country + " | " + country.count);
-// //           });
-  
-// //           dot.mouseout(() => {
-// //               dot.removeClass("hover");
-// //               $('#hoverLabel').text("");
-// //           });
-  
-// //           $('#stage').append(dot);
+    womenCountries = cumulatedGender.Female; //Datensatz mit Künstlerinnen sortiert nach Nationalität
+    womenCountries = gmynd.mergeData(womenCountries, countryData, "nationality"); //Koordinaten werden zu sortierten weiblichen Ländern hinzugefügt
 
-// // };
+    menCountries = cumulatedGender.Male; //Datensatz mit Künstler sortiert nach Nationalität
+    menCountries = gmynd.mergeData(menCountries, countryData, "nationality"); //Koordinaten werden zu sortierten männlichen Ländern hinzugefügt
 
-
-// function drawAll() {
-
-//  const artistsMax = gmynd.dataMax(allCountries, "count");
-
-//  allCountries.forEach(country => {
-//        showingAll = true;
-//        const area = gmynd.map(country.count, 0, artistsMax, 20, 1500);
-//        const r =  gmynd.circleRadius(area);
-//         //const area = 20;
-//         //const r =  gmynd.circleRadius(area);
-//         const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) -r;  //Längengrade
-//         const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
-
-//         let dot = $ ('<div></div>');
-//         dot.addClass("country");
-//           dot.css({
-//               'height': r * 2,
-//               'width': r * 2,
-//               'left': x,
-//               'top': y,
-//           });
-  
-//           dot.data(country);  //Daten in HTML Objekten speichern
-  
-//           dot.mouseover(() => {
-//               dot.addClass("hover");
-//               $('#hoverLabel').text(country.country + " | " + country.count);
-//           });
-  
-//           dot.mouseout(() => {
-//               dot.removeClass("hover");
-//               $('#hoverLabel').text("");
-//           });
-  
-//           $('#stage').append(dot);
-
-//       });
-
-// };
-
-// function drawWomen() {
-
-//  const womenMax = gmynd.dataMax(womenCountries, "count");
-
-//     womenCountries.forEach(country => {
-//         showingWomen = true;
-//         const area = gmynd.map(country.count, 0, womenMax, 20, 1500);
-//         const r =  gmynd.circleRadius(area);
-//         const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) -r;  //Längengrade
-//         const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
-
-//         let dot = $ ('<div></div>');
-//         dot.addClass("country");
-//           dot.css({
-//               'height': r * 2,
-//               'width': r * 2,
-//               'left': x,
-//               'top': y,
-//           });
-  
-//           dot.data(country);  //Daten in HTML Objekten speichern
-  
-//           dot.mouseover(() => {
-//               dot.addClass("hover");
-//               $('#hoverLabel').text(country.country + " | " + country.count);
-//           });
-  
-//           dot.mouseout(() => {
-//               dot.removeClass("hover");
-//               $('#hoverLabel').text("");
-//           });
-  
-//           $('#stage').append(dot);
-
-//     });
+    womenCountries = gmynd.renameProps(womenCountries, ["count"], ["femaleCount"]);
+    menCountries = gmynd.renameProps(menCountries, ["count"], ["maleCount"]);
+    allCountries = gmynd.renameProps(allCountries, ["count"], ["totalCount"]);
+    allCountries = gmynd.mergeData(allCountries, womenCountries, "alpha3Code");
+    allCountries = gmynd.mergeData(allCountries, menCountries, "alpha3Code");
+    allCountries = gmynd.deleteProps(allCountries, "gender");
     
-// };
+    //console.log(allCountries);
+};
 
-// function drawMen() {
-//  const menMax = gmynd.dataMax(menCountries, "count");
 
-//     menCountries.forEach(country => {
-//         showingMen = true;
-//         const area = gmynd.map(country.count, 0, menMax, 20, 1500);
-//         const r =  gmynd.circleRadius(area);
-//         const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) -r;  //Längengrade
-//         const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
+function menuAllView() {
 
-//         let dot = $ ('<div></div>');
-//         dot.addClass("country");
-//           dot.css({
-//               'height': r * 2,
-//               'width': r * 2,
-//               'left': x,
-//               'top': y,
-//           });
+    if (femaleMapShow || maleMapShow) {
+        $('.country').show();
+        $('.womenCountry').hide();
+        $('.menCountry').hide();
+        drawTotalMap();
+    };
+};
+
+function menuFemaleView() {
+    if (totalMapShow || maleMapShow) {
+        $('.country').hide();
+        $('.womenCountry').show();
+        $('.menCountry').hide();
+        drawFemaleMap();
+    };
+};
+
+function menuMaleView() {
+    if (totalMapShow || femaleMapShow) {
+        $('.country').hide();
+        $('.womenCountry').hide();
+        $('.menCountry').show();
+        drawMaleMap();
+    };
+};
+
+function createDots() {
+
+    const artistsMax = gmynd.dataMax(allCountries, "totalCount");
+    allCountries.forEach(country => {
+        const area = gmynd.map(country.totalCount, 0, artistsMax, 25, 1000);
+        const r = gmynd.circleRadius(area);
+        const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) - r;  //Längengrade
+        const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
+
+        let totalDot = $('<div></div>');
+        totalDot.addClass("country");
+        totalDot.css({
+            'height': r * 2,
+            'width': r * 2,
+            'left': x,
+            'top': y,
+        });
+
+        totalDot.data(country);  //Daten in HTML Objekten speichern
+
+        totalDot.mouseover(() => {
+            totalDot.addClass("hover");
+            $('#hoverLabel').text(country.country + " | " + country.totalCount);
+        });
+
+        totalDot.mouseout(() => {
+            totalDot.removeClass("hover");
+            $('#hoverLabel').text("");
+        });
+
+        $('#stage').append(totalDot);
+    });  
+
+    
+    const womenMax = gmynd.dataMax(allCountries, "femaleCount");
+    allCountries.forEach(country => {
+        const area = gmynd.map(country.femaleCount, 0, womenMax, 25, 1000);
+        const r = gmynd.circleRadius(area);
+        const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) - r;  //Längengrade
+        const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
+
+        let femaleDot = $('<div></div>');
+        femaleDot.addClass("womenCountry");
+        femaleDot.css({
+            'height': r * 2,
+            'width': r * 2,
+            'left': x,
+            'top': y,
+        });
+
+        femaleDot.data(country);  //Daten in HTML Objekten speichern
+
+        femaleDot.mouseover(() => {
+            femaleDot.addClass("hoverWomen");
+            $('#hoverLabel').text(country.country + " | " + country.femaleCount);
+        });
+
+        femaleDot.mouseout(() => {
+            femaleDot.removeClass("hoverWomen");
+            $('#hoverLabel').text("");
+        });
+
+        $('#stage').append(femaleDot);
+    });
+
+
+    const menMax = gmynd.dataMax(allCountries, "maleCount");
+    allCountries.forEach(country => {
+        const area = gmynd.map(country.maleCount, 0, menMax, 25, 1000);
+        const r = gmynd.circleRadius(area);
+        const x = gmynd.map(country.longitude, -180, 180, 0, stageWidth) - r;  //Längengrade
+        const y = gmynd.map(country.latitude, -90, 90, stageHeight, 0) - r;  // Breitengrade
+
+        let maleDot = $('<div></div>');
+        maleDot.addClass("menCountry");
+        maleDot.css({
+            'height': r * 2,
+            'width': r * 2,
+            'left': x,
+            'top': y,
+        });
+
+        maleDot.data(country);  //Daten in HTML Objekten speichern
+
+        maleDot.mouseover(() => {
+            maleDot.addClass("hoverMen");
+            $('#hoverLabel').text(country.country + " | " + country.maleCount);
+        });
+
+        maleDot.mouseout(() => {
+            maleDot.removeClass("hoverMen");
+            $('#hoverLabel').text("");
+        });
+
+        $('#stage').append(maleDot);
+    });
+
+};
+
+
+function drawTotalMap() { 
+    totalMapShow = true;
   
-//           dot.data(country);  //Daten in HTML Objekten speichern
+    $('.country').each(function() {
+      const totalDotData = $(this).data();
+      $(this).css({
+          'background-color': totalDotData.color,
+      });
+  });
   
-//           dot.mouseover(() => {
-//               dot.addClass("hover");
-//               $('#hoverLabel').text(country.country + " | " + country.count);
-//           });
-  
-//           dot.mouseout(() => {
-//               dot.removeClass("hover");
-//               $('#hoverLabel').text("");
-//           });
-  
-//           $('#stage').append(dot);
+};
 
-//        });
-
-// };
+function drawFemaleMap() { 
+    femaleMapShow = true;
   
-
-
-// function toggleView() {
-//     console.log("gedrückt");
-//      if (showingAll) {
-//        drawAll();
-//        };
-//      if (showingWomen) {
-//         drawWomen();
-//        };
-//      if (showingMen) {
-//           drawMen();
-//          };
-// };
+    $('.country').each(function() {
+      const femaleDotData = $(this).data();
+      $(this).css({
+          'background-color': femaleDotData.color,
+      });
+  });
   
+};
+
+function drawMaleMap() { 
+    maleMapShow = true;
+  
+    $('.country').each(function() {
+      const maleDotData = $(this).data();
+      $(this).css({
+          'background-color': maleDotData.color,
+      });
+  });
+  
+};
+
+
 // function clickHandler(event) {
 //     const dot = $(event.target);
 //     let contintent = dot.data().continent;
-     
+
 //     $('country[data-continent= "' + continent + '"]').css({
 //         'background-color': 'yellow',
 //     })
@@ -250,21 +230,3 @@
 //     $('#clickLabel').text(dot.data().countryName);  //Zugriff auf alle Paramenter von country Objekt (nicht nur countryName)
 // };
 
-// function drawMap() { 
-//     showingChart = false
-  
-//     $('.country').each(function() {
-//       const dotData = $(this).data();
-//       $(this).css({
-//           'background-color': dotData.color,
-//       });
-//       $(this).animate({
-//           height: dotData.mapHeight,
-//           width: dotData.mapWidth,
-//           'border-radius': 100,
-//           left: dotData.mapLeft,
-//           top: dotData.mapTop,
-//       }, 500);
-//   });
-
-// };
